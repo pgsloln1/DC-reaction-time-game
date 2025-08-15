9e77c40d5b72e191905d03e048cb2140c74377a0
pgsloln1 <103189237+pgsloln1@users.noreply.github.com>
2025-08-15 17:11:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -96,6 +96,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // health
 app.get('/healthz', (req, res) => res.json({ ok: true }));
 
